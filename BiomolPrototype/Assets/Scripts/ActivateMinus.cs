29c1b0bd8b61ec87c779ea05f224fd24cf5e75cc
Daniel Sá Barretto <daniel_barretto98@hotmail.com>
2018-06-24 22:02:14 -0300

    Add Lelvel Biomol

---
+++ BiomolPrototype/Assets/Scripts/ActivateMinus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMinus : MonoBehaviour {
+    private ActvatePlus ap;
+    public bool isActive = false;
+    private void Start()
+    {
+        ap = GameObject.FindGameObjectWithTag("Plus").GetComponent<ActvatePlus>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Clone"))
+        {
+            isActive = true;
+            if(ap.isActive && isActive)
+            {
+
+            }
+        }
+    }
+}
---
+++ BiomolPrototype/Assets/Scripts/ActvatePlus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActvatePlus : MonoBehaviour {
+
+    private ActivateMinus am;
+    public bool isActive = false;
+    
+    void Start()
+    {
+        am = GameObject.FindGameObjectWithTag("Minus").GetComponent<ActivateMinus>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Clone"))
+        {
+            isActive = true;
+            if (am.isActive && isActive)
+            {
+
+            }
+        }
+    }
+}
