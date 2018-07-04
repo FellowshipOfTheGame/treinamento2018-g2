cbc7d69d9e54ed5faf5516ab497e486a8427333b
fergorgs <39651597+fergorgs@users.noreply.github.com>
2018-07-04 10:34:04 -0300

    revert beto 2

--- G2/Assets/Resources/Magnetismo/Scripts/VerOnOff.cs
+++ G2/Assets/Resources/Magnetismo/Scripts/VerOnOff.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VerOnOff : Resp_Interacao
+public class VerOnOff : MonoBehaviour
 {
 
     private GameObject[] imas;
-
-    private Sprite sprite_on;
-    private Sprite sprite_off;
-
-    private SpriteRenderer sprRen;
 
     // Use this for initialization
     void Start()
@@ -18,41 +13,20 @@ public class VerOnOff : Resp_Interacao
 
         imas = GameObject.FindGameObjectsWithTag("Ima");
 
-        sprRen = GetComponent<SpriteRenderer>();
-
-        sprite_on = Resources.Load<Sprite>("Magnetismo/Sprites/clicado_ver");// as Sprite;
-        sprite_off = Resources.Load<Sprite>("Magnetismo/Sprites/normal_ver");// as Sprite;
-
     }
 
-
-    override public void Acao()
-    {
-
-        for (int i = 0; i < imas.Length; i++)
-        {
-            if (imas[i].GetComponent<Controle_Ima>().tipo == Controle_Ima.Tipo_Ima.Verde)
-                imas[i].GetComponent<Controle_Ima>().ativo = !imas[i].GetComponent<Controle_Ima>().ativo;
-
-        }
-
-        sprRen.sprite = sprite_on;
-
-        Invoke("ResetSprite", 1);
-
-    }
-
-    void ResetSprite()
-    {
-
-        sprRen.sprite = sprite_off;
-
-    }
-
+    // Update is called once per frame
     void Update()
     {
 
         if (Input.GetKeyDown(KeyCode.V))
-            Acao();
+        {
+            for (int i = 0; i < imas.Length; i++)
+            {
+                if (imas[i].GetComponent<Controle_Ima>().tipo == Controle_Ima.Tipo_Ima.Verde)
+                    imas[i].GetComponent<Controle_Ima>().ativo = !imas[i].GetComponent<Controle_Ima>().ativo;
+
+            }
+        }
     }
 }
