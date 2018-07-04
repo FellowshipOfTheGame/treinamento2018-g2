fb540134a24fda299ed2dd1de6a87e0c67ef1934
fergorgs <39651597+fergorgs@users.noreply.github.com>
2018-07-04 10:39:26 -0300

    revert beto 4

---
+++ BiomolPrototype/Assets/Scripts/CloneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneController : MonoBehaviour {
+    public float velocidade;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movimento = new Vector2(-moveHorizontal,-moveVertical);
+        GetComponent<Rigidbody>().velocity = movimento * velocidade;
+    }
+}
